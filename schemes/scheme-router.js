44d0116c62111676f11c2173ad76ad2cbd942e98
sami-aloosi <kingxsam@gmail.com>
2020-09-10 13:03:19 -0700

    sami-alaloosi

--- schemes/scheme-router.js
+++ schemes/scheme-router.js
@@ -14,6 +14,7 @@ router.get('/', (req, res) => {
   });
 });
 
+
 router.get('/:id', (req, res) => {
   const { id } = req.params;
 
--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const SchemeRouter = require('./schemes/scheme-router.js');
 const server = express();
 
 server.use(express.json());
+
+// to make sure that the server is up and running correctly
+server.get("/", (req, res)=>{
+    res.status(200).send(`<h1> Server is up and runnig </h1>`)
+})
+
 server.use('/api/schemes', SchemeRouter);
 
 module.exports = server;
